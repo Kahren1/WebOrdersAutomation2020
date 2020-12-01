5a756baff505b7ab76f271b426dd02490bca1967
Kahren1 <kahren.mkrtchyan@gmail.com>
2020-11-30 23:18:36 -0500

    updated driver class to run in parallel

--- src/test/java/com/weborders/utilities/Driver.java
+++ src/test/java/com/weborders/utilities/Driver.java
@@ -8,48 +8,53 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
+
+
+
+
     //create once, use multiple times
     //same for everyone
-    private static WebDriver driver; //so no one can create object of Driver class
+    private static ThreadLocal<WebDriver> driverPool=new ThreadLocal<>();
+
+   // private static WebDriver driver; //so no one can create object of Driver class
     //everyone should call static getter method instead
     private Driver(){
 
     }
     public static WebDriver getDriver(){
 
-        if(driver==null){
+        if(driverPool.get()==null){
             String browser = ConfigurationReader.getProperty("browser").toLowerCase();
+
             switch(browser){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--start-maximized");
-                    driver = new ChromeDriver(chromeOptions);
+                    driverPool.set(new ChromeDriver(chromeOptions));
                     break;
                 case "chromeheadless":
                     //to turn headless mode (without interface)
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
                     options.setHeadless(true);
-                    driver=new ChromeDriver(options);
+                    driverPool.set(new ChromeDriver(options));
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver=new FirefoxDriver();
+                    driverPool.set(new FirefoxDriver());
                     break;
                 default:
                     throw new RuntimeException("Wrong browser name!");
             }
-            return driver;
         }
-
-        return driver;
+        return driverPool.get();
     }
 
     public static void closeDriver(){
-        if(driver!=null){
-            driver.quit();
-            driver=null;
+        if(driverPool!=null){
+            driverPool.get().quit();
+            driverPool.remove();
         }
     }
 }
